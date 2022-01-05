646f662d8717c46b924b9d34be5a27747d013c80
Berk Disli <berkdisli@yahoo.com>
2022-01-05 14:13:48 +0100

    4.4

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+    state = {
+        query: '',
+        manualAmountEvents: 2
+      }
 
-  state = {
-    numberOfEvents: 32
-  }
-
-  handleInputChanged = (event) => {
-    const number = event.target.value;
-    if (number <= 0 || number > 32) {
-      return window.alert('Choose number between 1-32');
-    } else {
-      this.setState({ 
-        numberOfEvents: number,
+    toggleAmountOfEvents = (events) => {
+        const value = events.target.value;
+        this.setState({
+          manualAmountEvents : value
         });
-        this.props.updateNumberOfEvents(event.target.value);
-    }
-  };
+      };
 
-  render() {
-    return (
-      <div>
-        <p>Change number of shown events between 1-32</p>
-
-        <input type="number" id="numberInput" value={this.state.numberOfEvents} className="numberInput" onChange={() => this.handleInputChanged(e)} />
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div className="numberOfEvents">
+            <input 
+                type="number"
+                className="amountEventsOnePage"
+                value={this.state.manualAmountEvents}
+                onChange={this.toggleAmountOfEvents}
+            />
+          </div>
+        );
+      }
 }
 
 export default NumberOfEvents;
